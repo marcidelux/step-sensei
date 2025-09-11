ca8f5fae3575d2f8b45faa8fdd7b1e9fce17fa04
marcidelux <marton.lorinczi@gmail.com>
2025-09-12 01:30:52 +0200

    Add base to the astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://marcidelux.github.io/step-sensei',
+	base: '/step-sensei',
 	integrations: [
 		starlight({
 			title: 'StepSensei',
