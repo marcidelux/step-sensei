2441928f1a1ffff89df3b201a785a5e1e336a3c1
marcidelux <marton.lorinczi@gmail.com>
2025-09-12 01:14:52 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,74 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'StepSensei',
+			defaultLocale: 'hu',
+			tableOfContents: false,
+			locales: {
+				en: { label: 'English' },
+				hu: { label: 'Magyar', lang: 'hu' }
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Courses',
+					translations: {
+						'hu': 'Tanfolyamok',
+					},
+					items: [
+						{
+							label: 'Chess',
+							translations: {
+								'hu': 'Sakk',
+							},
+							slug: 'courses/chess'
+						},
+						{
+							label: 'Informatics',
+							translations: {
+								'hu': 'Informatika',
+							},
+							slug: 'courses/informatics'
+						},
+						{
+							label: 'Mathematics',
+							translations: {
+								'hu': 'Matematika',
+							},
+							slug: 'courses/matematics'
+						},
+					],
+				},
+				{
+					label: 'Mentors',
+					translations: {
+						'hu': 'Mentorok',
+					},
+					items: [
+						{
+							label: 'Bugaj Mirka',
+							translations: {
+								'hu': 'Bugaj Mirka',
+							},
+							slug: 'mentors/mirka'
+						},
+						{
+							label: 'Radnai Adam',
+							translations: {
+								'hu': 'Radnai Ádám',
+							},
+							slug: 'mentors/radnai-adam'
+						},
+					],
+				}
+
+			],
+			customCss: ['./src/styles/custom.css'],
+		}),
+	],
+});
